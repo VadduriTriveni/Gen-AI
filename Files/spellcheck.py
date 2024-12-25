d454a0628d2d6c7f4a36c23dc1e84775b97517ff
Vadduri Triveni <97962294+VadduriTriveni@users.noreply.github.com>
2024-12-25 14:57:44 +0530

    Update spellcheck.py

--- Files/spellcheck.py
+++ Files/spellcheck.py
@@ -1,15 +1,17 @@
-Call the TextBlob library. Create a new python file with the code below.
+Spell Check
+
+1.Call the TextBlob library. Create a new python file with the code below.
 from textblob import TextBlob
 
-Make a mistake. Create a text variable with a spelling error.
+2.Make a mistake. Create a text variable with a spelling error.
 text = 'I love progamming and machine learnig.'
 
-Create a TextBlob object with the text.
+3.Create a TextBlob object with the text.
 blob = TextBlob(text)
 
-Check your spelling
+4.Check your spelling
 corrected_text = blob.correct()
 
-# Print the corrected text
+5.# Print the corrected text
 print('Original Text:', text)
 print('Corrected Text:', corrected_text)
