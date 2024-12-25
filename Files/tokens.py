bf30a3f4d20a4a63bef9c4fcbecd4bca80846fc4
Vadduri Triveni <97962294+VadduriTriveni@users.noreply.github.com>
2024-12-25 14:41:46 +0530

    Create tokens.py

---
+++ Files/tokens.py
@@ -0,0 +1,18 @@
+ Tokenization
+
+In your code editor, create a new tokens.py file.
+
+Import the tokenize library with the following code:
+
+import nltk
+nltk.download('punkt_tab')
+from nltk.tokenize import word_tokenize
+
+Use the code below to perform a simple tokenization task:
+
+sample_text = 'I love programming!'
+tokens = word_tokenize(sample_text)
+
+print('Tokens:', tokens)
+
+Your output should return a list of strings that represent the tokens in the sample_text.
